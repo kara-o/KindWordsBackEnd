7e75c29207ea2f5b287221e5fa9ebceab02f135f
Justin Maple <justinamaple@gmail.com>
2019-11-07 19:26:35 -0800

    Able to create account through REST

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
 class AccountsController < ApplicationController
+  def create
+    account = Account.new(
+      email: params[:email],
+      password: params[:password],
+      icon: params[:email].initial
+    )
+
+    if account.save
+      session[:account_id] = account.id
+      # flash[:success] = 'Succesffuly Created Acccount!'
+    else
+      # flash[:warning] = 'Invalid Email/Password'
+    end
+  end
+end
+
+class String
+  def initial
+    self[0, 1].upcase!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :accounts
+  post '/login' => 'accounts#login'
+  post '/signup' => 'accounts#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
