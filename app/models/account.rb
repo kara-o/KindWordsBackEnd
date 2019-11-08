2a3eab071ef00edb6616319119bb0321c2acd524
Justin Maple <justinamaple@gmail.com>
2019-11-07 20:46:25 -0800

    Added Unit Tests for Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,5 +2,6 @@ class Account < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,8 +1,35 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  it 'is valid with valid attributes'
-  it 'is not valid without a email'
-  it 'is not valid without a password'
-  it 'is not valid without a icon'
+  subject do
+    described_class.new(email: 'test@test.com', password: 'Anything')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a email' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is invalid with malformed email' do
+    subject.email = 'test'
+    expect(subject).to_not be_valid
+  end
+
+  context 'when email is not unique' do
+    before { described_class.create!(email: 'test@test.com', password: 'Anything') }
+    it 'is not valid without a unique email' do
+      expect(subject).to be_invalid
+    end
+  end
 end
