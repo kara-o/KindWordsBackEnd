f21f820dc5d6a8f8325a107ca9314e0eabba1663
kara-o <44376276+kara-o@users.noreply.github.com>
2019-11-12 12:57:16 -0800

    basic controller tests

--- spec/controllers/letters_controller_spec.rb
+++ spec/controllers/letters_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe LettersController, type: :controller do
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
+
+  describe "POST create" do
+    it "has a 200 status code" do
+      post :create
+      expect(response.status).to eq(200)
+    end
+  end
 end
--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ResponsesController, type: :controller do
 
+  
+
 end
